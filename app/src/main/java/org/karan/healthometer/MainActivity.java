e36b175be8ee0c02b45783245b863e1a7cd22277
Karanvir Singh <karanvir.singh61@yahoo.in>
2017-11-22 19:52:05 +0530

    Last time I said it works? I was kidding. Try this.

--- app/src/main/java/org/karan/healthometer/MainActivity.java
+++ app/src/main/java/org/karan/healthometer/MainActivity.java
@@ -8,20 +8,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
 
     private TextView textView;
     private StepDetector simpleStepDetector;
     private SensorManager sensorManager;
     private Sensor accel;
-    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
+    private static final String TEXT_NUM_STEPS = "Steps: ";
     public long numSteps;
     private Button BtnStart, BtnStop, BtnReset;
-    private TextView TvSteps,CalorieView;
+    private TextView TvSteps,CalorieView, currentWaterValue, currentCaffeineValue, waterQuantity, caffeineQuantity;
+    public int waterGlasses,caffeineCups, currentWaterQuantity, currentCaffeineQuantity;
     public double Calories;
+    public ImageButton addWater, addCaffeine;
 
 
     @Override
@@ -34,6 +36,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         BtnStart = (Button) findViewById(R.id.btn_start);
         BtnStop = (Button) findViewById(R.id.btn_stop);
         BtnReset = (Button) findViewById(R.id.btn_reset);
+        addWater = (ImageButton) findViewById(R.id.addWater);
+        addCaffeine = (ImageButton) findViewById(R.id.addCaffeine);
+        currentWaterValue = (TextView) findViewById(R.id.currentWaterValue);
+        currentCaffeineValue = (TextView) findViewById(R.id.currentCaffeineValue);
+        waterQuantity = (TextView) findViewById(R.id.waterQuantity);
+        caffeineQuantity = (TextView) findViewById(R.id.caffeineQuantity);
 
         // Get an instance of the SensorManager
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -75,6 +83,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 BtnStop.setVisibility(View.INVISIBLE);
                 BtnStart.setVisibility(View.VISIBLE);
                 Toast.makeText(MainActivity.this, "Records cleared", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        addWater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                waterGlasses++;
+                currentWaterQuantity=currentWaterQuantity+250;
+                currentWaterValue.setText(""+waterGlasses);
+                waterQuantity.setText(currentWaterQuantity+" ml");
+            }
+        });
+
+        addCaffeine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                caffeineCups++;
+                currentCaffeineQuantity=currentCaffeineQuantity+80;
+                currentCaffeineValue.setText(""+caffeineCups);
+                caffeineQuantity.setText(currentCaffeineQuantity +" mg");
             }
         });
     }
