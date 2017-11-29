8efe6a1476f77bcecbdae87b3e7e1628963074af
Karanvir Singh <karanvir.singh61@yahoo.in>
2017-11-29 14:36:39 +0530

    It's Working!

--- app/src/main/java/org/karan/healthometerx/HealthNews.java
+++ app/src/main/java/org/karan/healthometerx/HealthNews.java
@@ -3,15 +3,25 @@ package org.karan.healthometerx;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.KeyEvent;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
-public class HealthNews extends AppCompatActivity {
-
+public class HealthNews extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     public WebView mywebView;
     public boolean internetIsUp=false;
     public ProgressBar progressBar;
@@ -19,7 +29,7 @@ public class HealthNews extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_health_news);
+        setContentView(R.layout.activity_health_news_with_drawer);
 
         mywebView = (WebView) findViewById(R.id.webview);
 
@@ -52,9 +62,69 @@ public class HealthNews extends AppCompatActivity {
         mywebView.setBackgroundColor(Color.TRANSPARENT);
         mywebView.setWebViewClient(new WebViewClient());
 
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    // To handle "Back" key press event for WebView to go back to previous screen.
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.health_news_with_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            HealthNews.this.finish();// Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event)
     {
--- app/src/main/java/org/karan/healthometerx/MainActivity.java
+++ app/src/main/java/org/karan/healthometerx/MainActivity.java
@@ -1,18 +1,27 @@
 package org.karan.healthometerx;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,SensorEventListener, StepListener  {
 
     private TextView textView;
     private StepDetector simpleStepDetector;
@@ -23,13 +32,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Button BtnStart, BtnStop, BtnReset;
     private TextView TvSteps,CalorieView, currentWaterValue, currentCaffeineValue, waterQuantity, caffeineQuantity;
     public int waterGlasses,caffeineCups, currentWaterQuantity, currentCaffeineQuantity;
-    public double Calories;
+    public float Calories;
     public ImageButton addWater, addCaffeine;
+    public SharedPreferences pref;
+    public SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         TvSteps = (TextView) findViewById(R.id.tv_steps);
         CalorieView = (TextView) findViewById(R.id.CalorieView);
@@ -43,6 +63,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         waterQuantity = (TextView) findViewById(R.id.waterQuantity);
         caffeineQuantity = (TextView) findViewById(R.id.caffeineQuantity);
 
+
+        pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        editor = pref.edit();
+        waterGlasses=pref.getInt("waterGlasses", 0);
+        currentWaterQuantity=pref.getInt("currentWaterQuantity", 0);
+        currentWaterValue.setText(""+waterGlasses);
+        waterQuantity.setText(currentWaterQuantity+" ml");
+
+        caffeineCups=pref.getInt("caffeineCups", 0);
+        currentCaffeineQuantity=pref.getInt("currentCaffeineQuantity", 0);
+        currentCaffeineValue.setText(""+caffeineCups);
+        caffeineQuantity.setText(currentCaffeineQuantity +" mg");
+
+        numSteps=pref.getLong("numSteps", 0);
+        Calories=pref.getFloat("Calories",0);
 
 
         // Get an instance of the SensorManager
@@ -81,6 +116,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 sensorManager.unregisterListener(MainActivity.this);
                 numSteps=0;
                 Calories=0;
+                editor.putLong("numSteps", 0);
+                editor.putFloat("Calories", 0);
+                editor.apply();
                 BtnReset.setVisibility(View.INVISIBLE);
                 BtnStop.setVisibility(View.INVISIBLE);
                 BtnStart.setVisibility(View.VISIBLE);
@@ -93,6 +131,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public void onClick(View arg0) {
                 waterGlasses++;
                 currentWaterQuantity=currentWaterQuantity+250;
+                editor.putInt("waterGlasses", waterGlasses);
+                editor.putInt("currentWaterQuantity", currentWaterQuantity);
+                editor.apply();
                 currentWaterValue.setText(""+waterGlasses);
                 waterQuantity.setText(currentWaterQuantity+" ml");
             }
@@ -103,10 +144,63 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             public void onClick(View arg0) {
                 caffeineCups++;
                 currentCaffeineQuantity=currentCaffeineQuantity+80;
+                editor.putInt("caffeineCups", caffeineCups);
+                editor.putInt("currentCaffeineQuantity", currentCaffeineQuantity);
+                editor.apply();
                 currentCaffeineValue.setText(""+caffeineCups);
                 caffeineQuantity.setText(currentCaffeineQuantity +" mg");
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        Intent i = new Intent(this,HealthNews.class);
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // code for home;
+        } else if (id == R.id.nav_gallery) {
+            // code for health_news
+            startActivity(i);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -124,8 +218,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void step(long timeNs) {
         numSteps++; //Stores value of steps
-        Calories = numSteps/(double)20; //Stores calories, algorithm by "Shape Up America!"
+        Calories = numSteps/(float)20; //Stores calories, algorithm by "Shape Up America!"
         TvSteps.setText(TEXT_NUM_STEPS + numSteps);
-        CalorieView.setText("Calories Burnt: "+ Double.toString(Calories)+" cal");
+        CalorieView.setText("Calories Burnt: "+ Float.toString(Calories)+" cal");
+        editor.putLong("numSteps", numSteps);
+        editor.putFloat("Calories", Calories);
+        editor.apply();
+
     }
 }
