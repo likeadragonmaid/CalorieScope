d9709057f8b8502035d02e83d1da2ce3959065fe
Karanvir Singh <karanvir.singh61@yahoo.in>
2018-01-03 19:19:33 +0530

    Fix setContentView

--- app/src/main/java/org/dynamicsoft/healthometer/HealthNews.java
+++ app/src/main/java/org/dynamicsoft/healthometer/HealthNews.java
@@ -26,7 +26,7 @@ public class HealthNews extends AppCompatActivity implements NavigationView.OnNa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_heathnews);
 
         mywebView = (WebView) findViewById(R.id.webview);
 
