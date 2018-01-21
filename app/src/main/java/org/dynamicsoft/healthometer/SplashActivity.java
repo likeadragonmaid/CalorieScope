fe6bbc46f66635d9a3e757056f88b6b6195a62aa
Karanvir Singh <karanvir.singh61@yahoo.in>
2018-01-21 14:23:46 +0530

    Partial Fixing of Splashscreen on older android devices

--- app/src/main/java/org/dynamicsoft/healthometer/SplashActivity.java
+++ app/src/main/java/org/dynamicsoft/healthometer/SplashActivity.java
@@ -1,24 +1,38 @@
 package org.dynamicsoft.healthometer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
-    private static int sp = 1000;
+public class SplashActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
         new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
                 finish();
             }
-        }, sp);
+        }, SPLASH_TIME_OUT);
     }
+
 }
