cb64ebc00f3fb5b42141cdaa807a6b6cde774e42
Karanvir Singh <karanvir.singh61@yahoo.in>
2018-01-21 16:50:29 +0530

    Dropped app:srcCompat in favor of android:src

--- app/src/main/java/org/dynamicsoft/healthometer/SplashActivity.java
+++ app/src/main/java/org/dynamicsoft/healthometer/SplashActivity.java
@@ -7,7 +7,6 @@ import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
-    // Splash screen timer
     private static int SPLASH_TIME_OUT = 3000;
 
     @Override
@@ -17,19 +16,10 @@ public class SplashActivity extends Activity {
 
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
                 Intent i = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(i);
-
-                // close this activity
                 finish();
             }
         }, SPLASH_TIME_OUT);
